02efaa133c761edd789f02117bfa97bbf41f4ccc
Shubham <shubhamaryan805@gmail.com>
2022-07-15 17:13:35 +0530

    Added Dice Roll to get between 1 To 6

--- src/Snake_Ladder.java
+++ src/Snake_Ladder.java
@@ -1,10 +1,19 @@
 package com.bridgelabz.javaproject;
 
+import java.util.Random;
+
 public class Snake_Ladder {
 
     static int position = 0;
 
+    public static int diceRoll() {
+        Random random = new Random();
+        int dice = random.nextInt(6) + 1;
+        return dice;
+    }
+
     public static void main(String[] args) {
+        System.out.println("dice= " + diceRoll());
 
     }
 
